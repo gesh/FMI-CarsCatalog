b15f8202c6a89f86c8a94b26d61c5284d420b703
I Os <addctdto@gmail.com>
2015-06-29 13:50:32 +0300

    fixed cars controller

--- CarsCatalog/CarsCatalog.Client/js/controllers/CarsController.js
+++ CarsCatalog/CarsCatalog.Client/js/controllers/CarsController.js
@@ -2,10 +2,8 @@
 
 app.controller('CarsController', function CarsController ($scope, carsDataSrv) {
 
-	//carsDataSrv.getData().then(function(data){
-	//	$scope.cars = data;
-	//	console.log($scope.cars);
-	//});
-
-	carsDataSrv.getAll();
+	carsDataSrv.getData().then(function(data){
+	    $scope.cars = data;
+	    console.log($scope.cars);
+	});
 });
