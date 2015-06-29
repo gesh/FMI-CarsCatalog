6d1260d4cf4c8bcfa49f33657f2c67679c82eee8
mivanov93 <mihail.ivanov93@gmail.com>
2015-06-29 06:13:10 -0700

    wddww
    wdw

--- CarsCatalog/CarsCatalog.Client/js/controllers/CarsController.js
+++ CarsCatalog/CarsCatalog.Client/js/controllers/CarsController.js
@@ -3,12 +3,7 @@
 app.controller('CarsController', function CarsController ($scope, carsDataSrv) {
 
 	carsDataSrv.getData().then(function(data){
-<<<<<<< HEAD
-	    $scope.cars = data;
-	    console.log($scope.cars);
-=======
+
 		$scope.cars = data;
-		console.log($scope.cars);
->>>>>>> parent of 4dd5a2e... cors for manufacturers
 	});
 });
