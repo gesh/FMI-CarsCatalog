cde320e9f2c90083c615d4695ce27b5040965de2
I Os <addctdto@gmail.com>
2015-06-29 15:57:19 +0300

    des

--- CarsCatalog/CarsCatalog.Client/js/controllers/AddCarController.js
+++ CarsCatalog/CarsCatalog.Client/js/controllers/AddCarController.js
@@ -3,15 +3,11 @@
 app.controller('AddCarController', function AddCarController($scope, carsDataSrv, usSpinnerService) {
 
     $scope.status = { done: false };
-    $scope.anotherOne = function () {
-        $scope.status.done = false;
-    };
-    $scope.startSpin = function () {
-        usSpinnerService.spin('spinner-add-cars');
-    }
-    $scope.stopSpin = function () {
-        usSpinnerService.stop('spinner-add-cars');
-    }
+
+    $scope.anotherOne = function () { $scope.status.done = false; };
+    $scope.startSpin = function () { usSpinnerService.spin('spinner-add-cars'); };
+    $scope.stopSpin = function () { usSpinnerService.stop('spinner-add-cars'); };
+
     $scope.postCarData = function (car, addCarForm) {
         if (!addCarForm.$valid)
         {
@@ -19,18 +15,22 @@ app.controller('AddCarController', function AddCarController($scope, carsDataSrv
             $scope.status.error = "Wrong data";
             //return;
         }
+
         $scope.status.error = "";
         $scope.status.inProg = true;
         $scope.startSpin();
+
 	    carsDataSrv.postData(car).then(function (data) {
 	        $scope.msg = "Done";
 	        $scope.status.done = true;
 	        $scope.status.inProg = false;
+	        $scope.status.error = 0;
 	        $scope.stopSpin();
 	    }, function () {
 	        $scope.msg = "Failed";
 	        $scope.status.done = true;
 	        $scope.status.inProg = false;
+	        $scope.status.error = 1;
 	        $scope.stopSpin();
 	    });
 	    
